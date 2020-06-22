329fb8db67f0acbd215b9ae046716a8eee4eb51a
YeZhenXin <6032508@qq.com>
2020-06-23 02:13:00 +0800

    初始化工程

---
+++ src/main/java/com/oioip/Springboot03StafFmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.oioip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03StafFmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot03StafFmanagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oioip/Springboot03StafFmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oioip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03StafFmanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
